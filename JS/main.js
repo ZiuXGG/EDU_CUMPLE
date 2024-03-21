e3b869ef8cf4bdf3691674f2008165c9b67ea898
ZiuXGG <151013459+ZiuXGG@users.noreply.github.com>
2024-03-21 17:23:09 +0100

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,18 @@
+document.getElementById('sendEmail').addEventListener('click', function() {
+    var recipient = 'eduard.varela@idc.cat';
+    var subject = encodeURIComponent('FELIÇ ANIVERSARI EDU!');
+    var body = encodeURIComponent('Que gaudeixis del teu dia i que el passis molt bé. 🎉');
+    var mailtoLink = 'https://mail.google.com/mail/?view=cm&to=' + recipient + '&su=' + subject + '&body=' + body;
+    
+    window.open(mailtoLink, 'ANIVERSARI EDU');
+  });
+document.getElementById('sendEmail1').addEventListener('click', function() {
+    var recipient = 'eduard.varela@idc.cat';
+    var subject = encodeURIComponent('FELIÇ ANIVERSARI EDU!');
+    var body = encodeURIComponent('Que gaudeixis del teu dia i que el passis molt bé. 🎉');
+    var mailtoLink = 'https://mail.google.com/mail/?view=cm&to=' + recipient + '&su=' + subject + '&body=' + body;
+    
+    window.open(mailtoLink, 'ANIVERSARI EDU');
+  });
+var cancion = document.getElementById("cancion");
+  cancion.volume = 0.5;

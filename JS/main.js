bd15a61e37efe74770c732996596388b7498afff
ZiuXGG <151013459+ZiuXGG@users.noreply.github.com>
2024-03-21 08:02:13 +0100

    Delete JS directory

--- JS/main.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('sendEmail').addEventListener('click', function() {
-    var recipient = 'eduard.varela@idc.cat';
-    var subject = encodeURIComponent('FELIÇ ANIVERSARI EDU!');
-    var body = encodeURIComponent('Que gaudeixis del teu dia i que et passi molt bé. 🎉');
-    var mailtoLink = 'https://mail.google.com/mail/?view=cm&to=' + recipient + '&su=' + subject + '&body=' + body;
-    
-    window.open(mailtoLink, 'ANIVERSARI EDU');
-  });
